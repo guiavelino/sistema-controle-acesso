e73b5ba11c2e298d23629d42909c3e4e1708c7dd
Jose Alisson <josealissonbezerra@hotmail.com>
2020-05-31 13:28:10 -0300

    Acrescentado validação de input

--- public/js/validation.js
+++ public/js/validation.js
@@ -39,6 +39,38 @@ function valida(){
     
 });
 
+document.getElementById('cad-cpf').addEventListener('change',
+
+function valida(){
+    var inputCpf = document.getElementById('cad-cpf').value;
+    var btn = document.querySelector('#registrar');
+    btn.disabled = true;
+    
+    teste = cpf(inputCpf);
+    if(teste){
+        document.getElementById('cad-invalid').style ="display: none";
+        document.getElementById('cad-cpf').style = "border-bottom: 2px solid #20B2AA!important;";
+        btn.disabled = false;
+    }
+    else{
+        document.getElementById('cad-cpf').style = "border-bottom: 2px solid red!important;";
+        document.getElementById('cad-invalid').style ="display: block";
+        btn.disabled = true;
+
+    }
+    
+});
+
+document.getElementById('select-documento-cadastro').addEventListener('change',
+    function apagaInput() {
+        document.getElementById('cad-cpf').value = "";
+    }
+);
+document.getElementById('select-documento-registro').addEventListener('change',
+    function apagaInput() {
+        document.getElementById('cpf').value = "";
+    }
+);
 // verdadeiros
 // alert(cpf(cpf = '825.566.405-02'), cpf);
 // console.log('825.566.405-02', cpf('825.566.405-02'));
