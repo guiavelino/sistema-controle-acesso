98b55e71e9173ac0f84e471cb1a85e2cbd24e06f
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-06-07 22:54:34 -0300

    Ajustando máscara de RG

--- public/js/mask-rg.js
+++ public/js/mask-rg.js
@@ -2,61 +2,54 @@
 
 // Modal de cadastro
 $("#select-uf-cadastro").change(function() {
-    if(this.value == "AC" || this.value == "AM" || this.value == "RO" || this.value == "RR" || this.value == "TO"){
+    if(this.value == "AP"){
+        // AP
+        // 999.999
+        $("#cad-rg").mask("999.999");
+    }
+    else if(this.value == "AC" || this.value == "AM" || this.value == "RO" || this.value == "RR" || this.value == "TO"){
         // UF: AC - AM - RO - RR - TO
         // Máscara: 99.999.9
         $("#cad-rg").mask("99.999.9");
-        
     }
     else if(this.value == "AL" || this.value == "DF" || this.value == "ES" || this.value == "GO" || this.value == "MS" || this.value == "PB" || this.value == "SE" || this.value == "PI" || this.value == "RN"){
         // UF: AL - DF - ES - GO - MS - PB - SE - PI - RN 
         // Máscara: 99.999.99
         $("#cad-rg").mask("99.999.99");
-        
     }
     else if(this.value == "PE"){
         // UF: PE
         // Máscara: 99.999.999
         $("#cad-rg").mask("99.999.999");
-        
     }
     else if(this.value == "MT" || this.value == "PR" || this.value == "SC"){
         // UF: MT - PR - SC
         // Máscara: 99.999.99-9
         $("#cad-rg").mask("99.999.99-9");
-        
     }
     else if(this.value == "MA" || this.value == "RJ" || this.value == "SP"){
         // UF: MA - RJ - SP
         // Máscara: 99.999.999-9
         $("#cad-rg").mask("99.999.999-9");
-        
     }
     else if(this.value == "PA"){
         // UF: PA
         // Máscara: 99.999.99-99
         $("#cad-rg").mask("99.999.99-99");
-        
     }
     else if(this.value == "BA"){
         // UF: BA
         // Máscara: 99.999.999-99
         $("#cad-rg").mask("99.999.999-99");
-        
     }
     else if(this.value == "RS"){
         // UF: RS
         // Máscara: 99-99.999.99-9
         $("#cad-rg").mask("99-99.999.99-9");
-        
     }
 });
 
-
-
 // Máscaras restantes
-
-// Ámapa - AP
 
 // UF:MG
 // Máscara: 99.999.999 (alfanumérico) 
@@ -67,56 +60,64 @@ $("#select-uf-cadastro").change(function() {
 // Máscara 1980>: 99.999.9-99
 // (dois ultimos digitos referentes ao ano de emissão)
 
-
-
 // Modal de registro
 $("#select-uf-registro").change(function() {
-    if(this.value == "AC" || this.value == "AM" || this.value == "RO" || this.value == "RR" || this.value == "TO"){
-        // UF: AC - AM - RO - RR - TO
-        // Máscara: 99.999.9
+    if(this.value == "AP"){
+        $("#rg").mask("999.999");
+    }
+    else if(this.value == "AC" || this.value == "AM" || this.value == "RO" || this.value == "RR" || this.value == "TO"){
         $("#rg").mask("99.999.9");
-        
     }
     else if(this.value == "AL" || this.value == "DF" || this.value == "ES" || this.value == "GO" || this.value == "MS" || this.value == "PB" || this.value == "SE" || this.value == "PI" || this.value == "RN"){
-        // UF: AL - DF - ES - GO - MS - PB - SE - PI - RN 
-        // Máscara: 99.999.99
         $("#rg").mask("99.999.99");
-        
     }
     else if(this.value == "PE"){
-        // UF: PE
-        // Máscara: 99.999.999
         $("#rg").mask("99.999.999");
-        
     }
     else if(this.value == "MT" || this.value == "PR" || this.value == "SC"){
-        // UF: MT - PR - SC
-        // Máscara: 99.999.99-9
         $("#rg").mask("99.999.99-9");
-        
     }
     else if(this.value == "MA" || this.value == "RJ" || this.value == "SP"){
-        // UF: MA - RJ - SP
-        // Máscara: 99.999.999-9
         $("#rg").mask("99.999.999-9");
-        
     }
     else if(this.value == "PA"){
-        // UF: PA
-        // Máscara: 99.999.99-99
         $("#rg").mask("99.999.99-99");
-        
     }
     else if(this.value == "BA"){
-        // UF: BA
-        // Máscara: 99.999.999-99
-        $("#rg").mask("99.999.999-99");
-        
+        $("#rg").mask("99.999.999-99"); 
     }
     else if(this.value == "RS"){
-        // UF: RS
-        // Máscara: 99-99.999.99-9
         $("#rg").mask("99-99.999.99-9");
-        
+    }
+});
+
+// Tela de edição
+$("#select-uf-registro").change(function() {
+    if(this.value == "AP"){
+        $("#rg-edit").mask("999.999");
+    }
+    else if(this.value == "AC" || this.value == "AM" || this.value == "RO" || this.value == "RR" || this.value == "TO"){
+        $("#rg-edit").mask("99.999.9");   
+    }
+    else if(this.value == "AL" || this.value == "DF" || this.value == "ES" || this.value == "GO" || this.value == "MS" || this.value == "PB" || this.value == "SE" || this.value == "PI" || this.value == "RN"){
+        $("#rg-edit").mask("99.999.99"); 
+    }
+    else if(this.value == "PE"){
+        $("#rg-edit").mask("99.999.999");
+    }
+    else if(this.value == "MT" || this.value == "PR" || this.value == "SC"){
+        $("#rg-edit").mask("99.999.99-9"); 
+    }
+    else if(this.value == "MA" || this.value == "RJ" || this.value == "SP"){
+        $("#rg-edit").mask("99.999.999-9");
+    }
+    else if(this.value == "PA"){
+        $("#rg-edit").mask("99.999.99-99");
+    }
+    else if(this.value == "BA"){
+        $("#rg-edit").mask("99.999.999-99");
+    }
+    else if(this.value == "RS"){
+        $("#rg-edit").mask("99-99.999.99-9");  
     }
 });
